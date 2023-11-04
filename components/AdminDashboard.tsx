6af49a46b510d592301e60428a83e0b471fe9ed4
anusheel <anusheel@gmail.com>
2023-11-04 12:34:58 +0000

    Commit from Stub AI

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -5,6 +5,7 @@ interface Order {
   description: string;
   dateOfPurchase: string;
   price: number;
+  status: string;
 }
 
 interface AdminDashboardProps {
@@ -19,6 +20,7 @@ const AdminDashboard: React.FC<AdminDashboardProps> = ({ orders }) => {
     description: '',
     dateOfPurchase: '',
     price: 0,
+    status: '',
   });
 
   const handleAddOrder = () => {
@@ -28,13 +30,14 @@ const AdminDashboard: React.FC<AdminDashboardProps> = ({ orders }) => {
       description: '',
       dateOfPurchase: '',
       price: 0,
+      status: '',
     });
     setIsAdding(false);
   };
 
   return (
-    <div className="flex flex-col w-full max-w-screen-lg">
-      <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
+    <div className="flex flex-col w-full max-w-screen-lg text-sm">
+      <h1 className="text-xl font-bold mb-4">Admin Dashboard</h1>
       <button
         className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={() => setIsAdding(true)}
@@ -49,15 +52,21 @@ const AdminDashboard: React.FC<AdminDashboardProps> = ({ orders }) => {
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date of Purchase</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
             {orderList.map((order, index) => (
-              <tr key={index} className="hover:bg-gray-100">
+              <tr key={index} className={`hover:bg-gray-100 ${index % 2 === 0 ? 'bg-blue-50' : ''}`}>
                 <td className="px-6 py-4 whitespace-nowrap">{order.title}</td>
                 <td className="px-6 py-4 whitespace-nowrap">{order.description}</td>
                 <td className="px-6 py-4 whitespace-nowrap">{order.dateOfPurchase}</td>
                 <td className="px-6 py-4 whitespace-nowrap">{order.price}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${order.status === 'Completed' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'}`}>
+                    {order.status}
+                  </span>
+                </td>
               </tr>
             ))}
             <tr className={`hover:bg-gray-100 ${isAdding ? '' : 'hidden'}`}>
@@ -97,6 +106,15 @@ const AdminDashboard: React.FC<AdminDashboardProps> = ({ orders }) => {
                   value={newOrder.price}
                   onChange={(e) =>
                     setNewOrder({ ...newOrder, price: Number(e.target.value) })
+                  }
+                />
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <input
+                  type="text"
+                  value={newOrder.status}
+                  onChange={(e) =>
+                    setNewOrder({ ...newOrder, status: e.target.value })
                   }
                 />
               </td>
